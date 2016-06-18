e2d454c6ad3ba9b58bc82db31a47590bb419cb6f
root <root@rivetops1.mylabserver.com>
2016-06-18 16:56:57 +0000

    First commit

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,2 @@
+default["apache"]["sites"]["rivetops2"]={"site_title" => "Website","port"=>80,"domain"=>"rivetops2.mylabserver.com"}
+default["apache"]["sites"]["rivetops2b"]={"site_title" => "Website2b","port"=>80,"domain"=>"rivetops2b.mylabserver.com"}
---
+++ cookbooks/security/recipes/default.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook Name:: security
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+#
+#i
+#
+#
+file "/etc/chef/validation.pem" do
+	action:delete
+end
